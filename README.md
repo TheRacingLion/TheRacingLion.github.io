715ddee04d1b6adeb4898e56baa01f87748d57c5
TheRacingLion <dev@trlcs.com>
2019-02-05 02:00:48 +0000

    :sparkles: | Start of TRLCS v2

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 <!---
-  Created by TheRacingLion (http://trlcs.com/) [ 17 / 04 / 2017 ]
+  Created by TheRacingLion (http://trlcs.com/) - 2019
   -*Read LICENSE to know more about permissions*-
 
   Readme File. Everything there is to know about this website.
 -->
 <div align="center">
   <h1 align="center">~ <a href="http://trlcs.com/">trlcs.com</a> ~</h1>
-    <strong>A personal website to showcase some projects and information about myself.</strong><br />
+  <strong>A personal website to showcase some projects and information about myself.</strong>
+  <br>
   <p align="center">
     <a href="http://forthebadge.com"><img src="http://forthebadge.com/images/badges/oooo-kill-em.svg"></a>
   <br>
--- js/main.js
+++ js/main.js
@@ -1,32 +1,121 @@
-(function ($) {
-    $(".navbar a[target!='_blank']").on('click', function (event) {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function () {
-            window.location.hash = hash;
-        });
-    });
-    $(window).scroll(function () {
-        if ($(".navbar-default").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-            $('#back-to-top').fadeIn();
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-            $('#back-to-top').fadeOut();
-        }
-    });
-    $('#back-to-top').click(function () {
-        $('#back-to-top').tooltip('hide');
-        $('body,html').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
-    $('#back-to-top').tooltip('show');
-    $('a#social-link').on('click', function (event) {
-        var win = window.open(this.attr('href'), '_blank');
-        win.focus();
-    });
-})(jQuery);
+particlesJS("background-particles", {
+  "particles": {
+    "number": {
+      "value": 300,
+      "density": {
+        "enable": true,
+        "value_area": 790
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": { "nb_sides": 5 }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": { "enable": false },
+    "move": {
+      "enable": false,
+      "speed": 0.5,
+      "direction": 'none',
+      "random": false,
+      "straight": false,
+      "out_mode": 'out',
+      "bounce": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": { "enable": false },
+      "onclick": { "enable": false },
+      "resize": true
+    }
+  },
+  "retina_detect": true,
+  "modes": {}
+});
+
+particlesJS("line-particles", {
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
+        "enable": true,
+        "value_area": 250
+      }
+    },
+    "color": {
+      "value": "#4B3820"
+    },
+    "shape": {
+      "type": ["polygon", "image"],
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": { "nb_sides": 5 },
+      "image": {
+        "src": "images/rock.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": { "enable": false }
+    },
+    "size": {
+      "value": 6,
+      "random": true,
+      "anim": { "enable": false }
+    },
+    "line_linked": { "enable": false },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "right",
+      "random": true,
+      "straight": true,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": { "enable": false }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": { "enable": false },
+      "onclick": { "enable": false },
+      "resize": true
+    }
+  },
+  "retina_detect": true,
+  "modes": {}
+});
